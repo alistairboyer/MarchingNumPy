9687eb8a4ab982c3e0be62c443ad25db262510c9
alistairboyer <alistairboyer@gmail.com>
2023-06-21 11:03:16 +0100

    add __all__

--- MarchingNumPy/__init__.py
+++ MarchingNumPy/__init__.py
@@ -2,3 +2,7 @@ from .MarchingCubesLorensen import marching_cubes_lorensen
 from .MarchingSquares import marching_squares
 from .MarchingTriangles import marching_triangles
 from .MarchingTriangles import marching_triangles_reversed
+
+__all__ = [
+    "MarchingNumPy",
+]
